476c657b8681d816584f8d0352e044f8011f48a5
xWxfFle <factores175@gmail.com>
2023-12-14 17:09:26 +0300

    feat: new model

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,14 +6,14 @@ import { Pickachu } from '@/shared/model-assets'
 export const App = () => {
   return (
     <div className="h-[100dvh]">
-      <Suspense fallback={null}>
-        <Canvas className="w-full h-full">
+      <Canvas className="w-full h-full bg-cyan-900">
+        <Suspense fallback={null}>
           <Stage>
             <Pickachu />
           </Stage>
-          <OrbitControls />
-        </Canvas>
-      </Suspense>
+          <OrbitControls enablePan={false} />
+        </Suspense>
+      </Canvas>
     </div>
   )
 }
