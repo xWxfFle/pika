b7496f31aa65d53473a5f8f9978a5d43f3b08678
xWxfFle <factores175@gmail.com>
2023-12-22 01:04:52 +0300

    feat: added controller

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,13 +3,24 @@ import {
   Environment,
   GizmoHelper,
   GizmoViewport,
+  KeyboardControls,
   Loader,
   OrbitControls,
 } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
-import { Physics } from '@react-three/rapier'
+import { Physics, RigidBody } from '@react-three/rapier'
+import Ecctrl from 'ecctrl'
 import { Suspense } from 'react'
 import { Pika } from '@/shared/model-assets'
+
+const keyboardMap = [
+  { name: 'forward', keys: ['ArrowUp', 'KeyW'] },
+  { name: 'backward', keys: ['ArrowDown', 'KeyS'] },
+  { name: 'leftward', keys: ['ArrowLeft', 'KeyA'] },
+  { name: 'rightward', keys: ['ArrowRight', 'KeyD'] },
+  { name: 'jump', keys: ['Space'] },
+  { name: 'run', keys: ['Shift'] },
+]
 
 export const App = () => {
   return (
@@ -17,11 +28,21 @@ export const App = () => {
       <Canvas className="w-full h-full" camera={{ position: [0, 0, 2] }}>
         <Suspense>
           <ambientLight intensity={0.5} />
-          <Physics gravity={[0, 1, 0]} colliders={false} debug>
-            <Pika />
-            <group position={[0, -0.05, 0]}>
-              <ContactShadows blur={2} />
-            </group>
+          <Physics timeStep="vary" debug>
+            <KeyboardControls map={keyboardMap}>
+              {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+              {/* @ts-ignore-next-line */}
+              <Ecctrl>
+                <Pika position={[0, -0.55, 0]} />
+              </Ecctrl>
+            </KeyboardControls>
+            <RigidBody restitution={2} />
+
+            <RigidBody type="fixed" colliders="trimesh">
+              <group position={[0, -0.05, 0]}>
+                <ContactShadows blur={2} />
+              </group>
+            </RigidBody>
           </Physics>
 
           <Environment
